43f0b889aeb27c7a210ad4b32eeecc2f679f2521
Noah Van Vaerenbergh <vvnoah@hotmail.com>
2022-11-21 18:47:35 +0100

    added ngswitch

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,17 +4,22 @@ import { Component, Input } from "@angular/core";
     selector: "event-thumbnail",
     template: `
         <div class="well hoverwell thumbnail">
-            <h2>{{event.name}}</h2>
-            <div>Date: {{event.date}}</div>
-            <div>Time: {{event.time}}</div>
-            <div>Price: €{{event.price}}</div>
-            <div>
-                <span>Location: {{event.location.address}}</span>
-                <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+            <h2>{{event?.name}}</h2>
+            <div>Date: {{event?.date}}</div>
+            <div [ngSwitch]="event?.time">Time: {{event?.time}}
+                <span *ngSwitchCase="'8:00 am'">(Early start)</span>
+                <span *ngSwitchCase="'10:00 am'">(Late start)</span>
+                <span *ngSwitchDefault>(Normal start)</span>
+            </div>
+            <div>Price: €{{event?.price}}</div>
+            <div *ngIf="event?.location">
+                <span>Location: {{event?.location?.address}}</span>
+                <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
             </div>
         </div>
     `,
     styles: [`
+        .thumbnail {min-height: 210px;}
         .pad-left { margin-left: 10px;}
         .well div {color: #bbb;}
     `]
