74f0d0ca612ad1bba3459d62bff727743fbbdc0f
Noah Van Vaerenbergh <vvnoah@hotmail.com>
2022-11-21 18:11:43 +0100

    added style to navbar component

--- src/app/header/navbar.component.ts
+++ src/app/header/navbar.component.ts
@@ -45,8 +45,15 @@ import { Component } from "@angular/core";
                 </div>
             </div>
         </div>
-    `
+    `,
+    styles: [`
+        .nav.navbar-nav {font-size: 15px;}
+        #searchForm {margin-right: 100px;}
+        @media (max-width: 1200px) {
+            #searchForm{display: none;}
+        }
+    `]
 })
 export class NavBarComponent{
-    
+
 }
