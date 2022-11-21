20e3ac0aa23687faaae4f70fbc3305566a4a46fc
Noah Van Vaerenbergh <vvnoah@hotmail.com>
2022-11-21 15:37:28 +0100

    Created event list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Hello world</h1>
-    <router-outlet></router-outlet>
-  `,
-  styles: []
+    <events-list></events-list>
+  `
 })
 export class AppComponent {
   title = 'angular-fundementals';
---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'events-list',
+    template: `
+        <div>
+            <h1>Upcoming Events</h1>
+        </div>
+    `
+})
+export class EventsListComponent {
+    
+}
