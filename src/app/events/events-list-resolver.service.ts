e4f973a465d2b0b1cd2ce5fe7eae94408e29e24f
Noah Van Vaerenbergh <vvnoah@hotmail.com>
2022-11-28 17:32:59 +0100

    created resolver for loading data in the route

---
+++ src/app/events/events-list-resolver.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
+import { EventService } from "./shared/event.service";
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class EventsListResolver implements Resolve<any> {
+    constructor(private eventService:EventService){}
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        return this.eventService.getEvents().pipe(map(events => events));
+    }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -3,11 +3,12 @@ import { Error404Component } from './app/errors/404.component';
 import { CreateEventComponent } from './app/events/create-events.component';
 import { EventDetailsComponent } from "./app/events/event-details/event-details.component";
 import { EventRouteActivator } from './app/events/event-route-activator.service';
+import { EventsListResolver } from './app/events/events-list-resolver.service';
 import { EventsListComponent } from "./app/events/events-list.component";
 
 export const ROUTES:Routes = [
     {path:'events/new', component: CreateEventComponent, canDeactivate:['canDeactivateCreateEvent']},
-    {path:'events', component: EventsListComponent},
+    {path:'events', component: EventsListComponent, resolve: {events:EventsListResolver}},
     {path:'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator]},
     {path:'404', component: Error404Component},
     {path:'', redirectTo: '/events', pathMatch: 'full'},
